8cc5c70bd3069359c680dcfd973e5a73f65b304f
DeerProjects <emreelbir0@protonmail.com>
2025-10-22 09:57:50 +0200

    CreatingFileModels
    Created File Locations and other things to create file models and started with db context and models

--- obj/Debug/net9.0/BikeRental.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/BikeRental.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BikeRental.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56943a2c689b1e55404bd7066aee57584b2bb6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BikeRental.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BikeRental.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
